ec1e3d074c46d0d4f8552a7c072da4a2a54428be
Rasmus Iversen <rassah@gmail.com>
2018-03-21 18:22:29 +0100

    Update DecoratorResult.java
    Adding state to decorator result to make it able to pass more information than simple should or shouldn't draw a decorator.

--- library/src/main/java/com/prolificinteractive/materialcalendarview/DecoratorResult.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/DecoratorResult.java
@@ -3,9 +3,15 @@ package com.prolificinteractive.materialcalendarview;
 class DecoratorResult {
     public final DayViewDecorator decorator;
     public final DayViewFacade result;
-
+    public final int state;
+    
     DecoratorResult(DayViewDecorator decorator, DayViewFacade result) {
+        this(decorator, result, 1);
+    }
+
+    DecoratorResult(DayViewDecorator decorator, DayViewFacade result, int state) {
         this.decorator = decorator;
         this.result = result;
+        this.state = state;
     }
 }
